582901ef739986101203c7d1941932bf0d7b0e8b
Giang Le <giang.le@aeris.net>
2024-08-10 21:30:59 +0700

    Change Mock battle sheet structure

--- src/main/java/com/jkmedia/ff7ecguildbot/service/GoogleSheetsServiceImpl.java
+++ src/main/java/com/jkmedia/ff7ecguildbot/service/GoogleSheetsServiceImpl.java
@@ -6,11 +6,16 @@ import com.jkmedia.ff7ecguildbot.GoogleSheetUtil;
 import java.io.*;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GoogleSheetsServiceImpl implements GoogleSheetsService {
 
   private final Sheets sheetsService;
@@ -18,9 +23,35 @@ public class GoogleSheetsServiceImpl implements GoogleSheetsService {
 
   @Override
   public void updateMockBattle(String username, int stage, double percentage) throws IOException {
-    updateCell("A2", username);
-    String range = GoogleSheetUtil.columnNumberToLetter(stage + 1) + "2";
-    updateCell(range, percentage);
+    Integer userRowNum = searchUser(username);
+    updateCell("A" + userRowNum, username);
+    String stageRange = GoogleSheetUtil.columnNumberToLetter(stage + 1) + userRowNum;
+    updateCell(stageRange, percentage);
+  }
+
+  private @NotNull Integer searchUser(String username) throws IOException {
+    ValueRange result =
+        sheetsService.spreadsheets().values().get(spreadsheetId, "A2:A31").execute();
+    Integer userRowNum = null;
+    if (CollectionUtils.isEmpty(result.getValues())) { // no user yet
+      userRowNum = 2;
+    } else {
+      int currentRow = 1; // first row is a header
+      for (List<Object> row : result.getValues()) {
+        currentRow++;
+        Object cell = row.get(0);
+        if (cell != null && Objects.equals(username, cell.toString())) {
+          userRowNum = currentRow;
+          break;
+        }
+      }
+      log.info("userRowNum: {}, currentRow: {}", userRowNum, currentRow);
+
+      if (userRowNum == null) { // user not found
+        userRowNum = currentRow + 1;
+      }
+    }
+    return userRowNum;
   }
 
   private void updateCell(String range, Object value) throws IOException {
