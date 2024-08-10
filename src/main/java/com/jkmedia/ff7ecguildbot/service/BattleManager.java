3a4efc3268bc1bc0039d08f1ed307fa2f74c0841
Giang Le <giang.le@aeris.net>
2024-08-10 22:53:08 +0700

    format code

--- src/main/java/com/jkmedia/ff7ecguildbot/service/BattleManager.java
+++ src/main/java/com/jkmedia/ff7ecguildbot/service/BattleManager.java
@@ -1,10 +1,8 @@
 package com.jkmedia.ff7ecguildbot.service;
 
 import com.jkmedia.ff7ecguildbot.object.MockBattleReport;
-
-import java.util.List;
 import java.util.SortedSet;
 
 public interface BattleManager {
-    void recommendStage(SortedSet<MockBattleReport> reports);
+  void recommendStage(SortedSet<MockBattleReport> reports);
 }
--- src/main/java/com/jkmedia/ff7ecguildbot/service/BattleManagerImpl.java
+++ src/main/java/com/jkmedia/ff7ecguildbot/service/BattleManagerImpl.java
@@ -1,10 +1,8 @@
 package com.jkmedia.ff7ecguildbot.service;
 
 import com.jkmedia.ff7ecguildbot.object.MockBattleReport;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 import java.util.SortedSet;
+import org.springframework.stereotype.Service;
 
 @Service
 public class BattleManagerImpl implements BattleManager {
