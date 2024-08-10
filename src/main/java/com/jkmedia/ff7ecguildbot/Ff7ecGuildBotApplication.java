cff9132c24bd6d9b9b421b1612ff8c65665a7f36
Giang Le <giang.le@aeris.net>
2024-08-10 11:07:50 +0700

    Initial commit

---
+++ src/main/java/com/jkmedia/ff7ecguildbot/Ff7ecGuildBotApplication.java
@@ -0,0 +1,13 @@
+package com.jkmedia.ff7ecguildbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ff7ecGuildBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ff7ecGuildBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jkmedia/ff7ecguildbot/Ff7ecGuildBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkmedia.ff7ecguildbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ff7ecGuildBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
