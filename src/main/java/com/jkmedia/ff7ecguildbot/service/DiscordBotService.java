60ce31add538544edcb5d3a8d327a108a1e730a4
Giang Le <giang.le@aeris.net>
2024-08-15 23:01:18 +0700

    reply set permission for admin-mock command

--- src/main/java/com/jkmedia/ff7ecguildbot/service/DiscordBotService.java
+++ src/main/java/com/jkmedia/ff7ecguildbot/service/DiscordBotService.java
@@ -7,7 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -40,15 +43,20 @@ public class DiscordBotService {
             Option.USERNAME.getValue(),
             "Discord display name of the user that you want to report for",
             true);
+    ;
 
     jda.upsertCommand(SlashCommand.MOCK.getValue(), "Submit mock battle result")
         .addOptions(stageOption, percentageOption)
         .queue();
 
     jda.upsertCommand(
-            SlashCommand.ADMIN_MOCK.getValue(),
-            "Submit mock battle result for other people (admin permission required)")
-        .addOptions(usernameOption, stageOption, percentageOption)
+            Commands.slash(
+                    SlashCommand.ADMIN_MOCK.getValue(),
+                    "Submit mock battle result for other people (admin permission required)")
+                .setDefaultPermissions(
+                    DefaultMemberPermissions.enabledFor(
+                        Permission.MANAGE_CHANNEL, Permission.MODERATE_MEMBERS))
+                .addOptions(usernameOption, stageOption, percentageOption))
         .queue();
   }
 
